92ec688bc29b0ff84f12d4c7d2ec41b472fce963
明明 <oneday2321@163.com>
2022-10-14 20:18:23 +0800

    testNG参数化读取

--- src/main/java/com/excelTest/readDemo.java
+++ src/main/java/com/excelTest/readDemo.java
@@ -1,10 +1,11 @@
 package com.excelTest;
 
 import com.alibaba.excel.EasyExcel;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class readDemo {
@@ -14,8 +15,18 @@ public class readDemo {
 
 //        test1(fileName,sheetName);
         String[][] strings = test2(read(fileName, sheetName));
-        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.deepToString(strings));
 
+    }
+    @DataProvider(name = "strings")
+    public Object[][] data(){
+        String fileName = ".//DataTest//caseData.xlsx";
+        String sheetName = "Sheet1";
+        return test2(read(fileName, sheetName));
+    }
+    @Test(dataProvider = "strings")
+    public void test11(String a1,String a2,String a3,String a4,String a5,String a6,String a7,String a8,String a9,String a10,String a11,String a12){
+        System.out.println(a1+"  "+a2+"  "+a3+"  "+a4+"  "+a5+"  "+a6+"  "+a7+"  "+a8+"  "+a9+"  "+a10+"  "+a11+"  "+a12);
     }
 
     public static List<excelDemo> read(String fileName, String sheetName) {
@@ -27,48 +38,11 @@ public class readDemo {
         return EasyExcel.read(fileName, excelDemo.class, new listenerDemo()).sheet(sheetName).doReadSync();
     }
 
-    public static HashMap<Integer, excelDemo> mapExcelCreate(String fileName, String sheetName) {
-        /*
-         * @Description 将excel对象集合存放至一个map中
-         * @Params  文件名称、sheet页名称
-         * @Return 返回map， 序号，excelDemo对象
-         */
-        List<excelDemo> read = read(fileName, sheetName);
-        HashMap<Integer, excelDemo> map = new HashMap<>();
-        for (int i = 0; i < read.size(); i++) {
-            map.put(i, read.get(i));
-        }
-        return map;
-    }
-
-
-    public static void test1(String fileName, String sheetName) {
-        HashMap<Integer, excelDemo> map = mapExcelCreate(fileName, sheetName);
-//        获取map中的一个对象，反射获取对象中所有属性
-        excelDemo excelDemo = map.get(1);
-        Field[] fields = excelDemo.getClass().getDeclaredFields();
-//        输出这个类中的所有属性 和 属性 对应的属性值
-        try {
-            for (Field field : fields) {
-                field.setAccessible(true);
-                String name = field.getName();
-                String o = (String) field.get(excelDemo);
-                if (o != null) {
-                    System.out.println(name + " == " + o);
-                } else {
-                    System.out.println(name + "--》对应的对象是空值");
-                }
-            }
-        } catch (IllegalAccessException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-
     //    获取返回集合，根据集合中对象获取其对应的变量值，以及对应的属性值
     public static String[][] test2(List<excelDemo> read) {
+        int length = excelDemo.class.getDeclaredFields().length;
         int size = read.size();
-        String[][] arr = new String[size][];
+        String[][] arr = new String[size][length];
         for (int i = 0; i < size; i++) {
             excelDemo demo = read.get(i);
             Field[] fields = demo.getClass().getDeclaredFields();
@@ -77,13 +51,11 @@ public class readDemo {
                     Field field = fields[j];
                     field.setAccessible(true);
                     String o = (String) field.get(demo);
-                    System.out.println(o);
                     if (o != null) {
                         arr[i][j] = o;
                     } else {
                         arr[i][j] = "";
                     }
-//                    arr[i][j] = o;
                 }
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
